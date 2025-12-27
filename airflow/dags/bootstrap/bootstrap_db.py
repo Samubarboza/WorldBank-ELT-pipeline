cfb737c0126c0a3d5f3b825d4e846bbaf8919602
Samuel Barboza <samu_junior95@hotmail.com>
2025-12-27 14:17:24 -0300

    elimine la extencion que genera id unicos del script bootstrap, decidi no utilizarlo

--- airflow/dags/bootstrap/bootstrap_db.py
+++ airflow/dags/bootstrap/bootstrap_db.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime
 
+# este dag lee la el script sql que crea los schema y prepara configuraciones para la base de datos
 with DAG(
     dag_id="00_bootstrap_db",
     start_date=datetime(2024, 1, 1),
@@ -14,6 +15,7 @@ with DAG(
     ]
 ) as dag:
 
+    # tarea que ejecuta el dag - se conecta a postgres y ejecuta el script bootstrap.sql.
     bootstrap = PostgresOperator(
         task_id="bootstrap_database",
         postgres_conn_id="worldbank_postgres",
--- scripts/bootstrap.sql
+++ scripts/bootstrap.sql
@@ -1,8 +1,4 @@
--- esto prepara la base de datos para el pipeline ELT creando los schemas y configuraciones basicas
--- crear schemas base del proyecto
+-- preparamos la base de datos -  creamos schemas para cada cada cosa, separar responsabilidades y no tener todo mezclado en la base de datos
 CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS stg;
 CREATE SCHEMA IF NOT EXISTS mart;
-
--- extensiones utiles (seguras y comunes)
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
